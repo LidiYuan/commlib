a5765682ca3bc3333d7dfe6784d1535079eae9b6
yuanlidi <yldfree@163.com>
2020-01-16 18:13:52 +0800

    clen file

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 rm -rf  aclocal.m4  autom4te.cache  configure m4 Makefile.in
+rm -rf src/config.h.in
+rm -rf src/Makefil.in
+rm -rf src/example/Makefile.in
+rm -rf src/mylib4c/Makefile.in
